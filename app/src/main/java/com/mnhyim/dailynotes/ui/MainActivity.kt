40b0d9abec37f5241bb81da436647a051260ae6e
Muhamad Nur Hasyim <mnhyim76@gmail.com>
2022-04-16 16:36:09 +0800

    fab listener

--- app/src/main/java/com/mnhyim/dailynotes/ui/MainActivity.kt
+++ app/src/main/java/com/mnhyim/dailynotes/ui/MainActivity.kt
@@ -6,16 +6,18 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.mnhyim.dailynotes.R
 import com.mnhyim.dailynotes.databinding.ActivityMainBinding
+import com.mnhyim.dailynotes.databinding.FragmentHomeBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
     private val TAG: String = MainActivity::class.java.simpleName.toString()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setupBottomNavBar()
--- app/src/main/java/com/mnhyim/dailynotes/ui/home/HomeFragment.kt
+++ app/src/main/java/com/mnhyim/dailynotes/ui/home/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.mnhyim.dailynotes.databinding.FragmentHomeBinding
 
@@ -17,6 +18,11 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        binding.fabAddNote.setOnClickListener {
+            Toast.makeText(context, "TESTT", Toast.LENGTH_LONG).show()
+        }
+
         return binding.root
     }
 
