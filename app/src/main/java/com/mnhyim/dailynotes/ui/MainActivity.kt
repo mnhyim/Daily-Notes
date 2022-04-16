d6bc65275dc8c1e200a7448b47e6aac070fe04cd
Muhamad Nur Hasyim <mnhyim76@gmail.com>
2022-04-16 15:41:14 +0800

    add bottom nav bar

--- app/src/main/java/com/mnhyim/dailynotes/ui/MainActivity.kt
+++ app/src/main/java/com/mnhyim/dailynotes/ui/MainActivity.kt
@@ -2,11 +2,29 @@ package com.mnhyim.dailynotes.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.mnhyim.dailynotes.R
+import com.mnhyim.dailynotes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val TAG: String = MainActivity::class.java.simpleName.toString()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupBottomNavBar()
+    }
+
+    private fun setupBottomNavBar() {
+        val bottomNavigationView = binding.bottomNavView
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.bottom_nav_container) as NavHostFragment
+        val navController = navHostFragment.navController
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
